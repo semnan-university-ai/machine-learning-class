e1acb3abae6fbdac2d0a503fa095af6b76ce51b5
fatemehmirzadeh <94124607+fatemehmirzadeh@users.noreply.github.com>
2021-12-15 00:45:18 -0800

    Add files via upload

---
+++ excersiecs/fatemehmirzadeh/15/15-nor.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 00:37:25 2021
+
+@author: central
+"""
+
+#nor
+def funcnor(A, B): 
+    if(A == 0) and (B == 0): 
+        return 1
+    else: 
+        return 0  
---
+++ excersiecs/fatemehmirzadeh/15/15-or.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 00:22:49 2021
+
+@author: central
+"""
+
+#or
+def funcor(a, b): 
+    if a == 1: 
+        return 1
+    elif b == 1: 
+        return 1
+    else: 
+        return 0
+    
+    print (funcor(0,1))
